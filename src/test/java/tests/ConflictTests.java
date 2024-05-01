4f9cb1f7319cec07bf1ae0b8d1f85225b07a0488
codewiser116 <codewise.academy@gmail.com>
2024-05-01 11:47:33 -0500

    modified test2 method

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -14,7 +14,7 @@ public class ConflictTests {
 
     @Test
     public void test2(){
-        String message = "Welcome to USA!";
+        String message = "Welcome to USA - the land of opportunities!";
         Assert.assertTrue(message.toLowerCase().contains("usa"));
     }
 
